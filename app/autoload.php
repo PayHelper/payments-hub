85c6110b7979a9640a695c7c59d6d5b842f45373
Rafał Muszyński <rmuszynski1@gmail.com>
2017-06-12 13:29:33 +0200

    behat suites

---
+++ app/autoload.php
@@ -0,0 +1,9 @@
+<?php
+use Composer\Autoload\ClassLoader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+/**
+ * @var ClassLoader
+ */
+$loader = require __DIR__.'/../vendor/autoload.php';
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+return $loader;
