f087bc5bc8dea8d5b8a07579ea443b09680d9c7f
Rafał Muszyński <rmuszynski1@gmail.com>
2017-07-26 12:02:38 +0200

    fixed order payload sending after order update (#8)
    * fixed order payload sending after order update
    
    * fix

--- src/PH/Bundle/CoreBundle/EventSubscriber/SendOrderPayloadListener.php
+++ src/PH/Bundle/CoreBundle/EventSubscriber/SendOrderPayloadListener.php
@@ -4,37 +4,26 @@ declare(strict_types=1);
 
 namespace PH\Bundle\CoreBundle\EventSubscriber;
 
-use GuzzleHttp\Client;
-use JMS\Serializer\SerializationContext;
-use JMS\Serializer\SerializerInterface;
+use PH\Bundle\CoreBundle\Sender\OrderPayloadSenderInterface;
 use PH\Component\Core\Model\OrderInterface;
-use PH\Component\Webhook\Model\WebhookInterface;
 use Sylius\Component\Resource\Exception\UnexpectedTypeException;
-use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
 final class SendOrderPayloadListener
 {
     /**
-     * @var RepositoryInterface
+     * @var OrderPayloadSenderInterface
      */
-    private $webhookRepository;
-
-    /**
-     * @var SerializerInterface
-     */
-    private $serializer;
+    private $orderPayloadSender;
 
     /**
      * SendOrderPayloadListener constructor.
      *
-     * @param RepositoryInterface $webhookRepository
-     * @param SerializerInterface $serializer
+     * @param OrderPayloadSenderInterface $orderPayloadSender
      */
-    public function __construct(RepositoryInterface $webhookRepository, SerializerInterface $serializer)
+    public function __construct(OrderPayloadSenderInterface $orderPayloadSender)
     {
-        $this->webhookRepository = $webhookRepository;
-        $this->serializer = $serializer;
+        $this->orderPayloadSender = $orderPayloadSender;
     }
 
     /**
@@ -46,17 +35,6 @@ final class SendOrderPayloadListener
             throw new UnexpectedTypeException($subject, OrderInterface::class);
         }
 
-        $context = new SerializationContext();
-        $context->setSerializeNull(true);
-
-        $destinations = $this->webhookRepository->findAll();
-
-        $client = new Client();
-        /** @var WebhookInterface $destination */
-        foreach ($destinations as $destination) {
-            $client->request('POST', $destination->getUrl(), [
-                'body' => $this->serializer->serialize($subject, 'json', $context),
-            ]);
-        }
+        $this->orderPayloadSender->send($subject);
     }
 }
