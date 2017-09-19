853ba5d405da090f81a80b9ee4774af7b1c18f0e
Rafał Muszyński <rmuszynski1@gmail.com>
2017-09-19 15:23:59 +0200

    Redirect improvements after the payment is canceled

---
+++ src/PH/Bundle/PayumBundle/Action/ResolveNextRouteAction.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PH\Bundle\PayumBundle\Action;
+
+use Payum\Core\Action\ActionInterface;
+use PH\Bundle\PayumBundle\Request\ResolveNextRoute;
+use PH\Component\Core\Model\PaymentInterface;
+
+final class ResolveNextRouteAction implements ActionInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param ResolveNextRoute $request
+     */
+    public function execute($request): void
+    {
+        /** @var PaymentInterface $payment */
+        $payment = $request->getFirstModel();
+
+        if (
+            $payment->getState() === PaymentInterface::STATE_COMPLETED ||
+            $payment->getState() === PaymentInterface::STATE_AUTHORIZED ||
+            $payment->getState() === PaymentInterface::STATE_PROCESSING
+        ) {
+            $request->setRouteName('ph_core_thankyou');
+            $request->setRouteParameters(['token' => $payment->getOrder()->getTokenValue()]);
+
+            return;
+        }
+
+        $request->setRouteName('ph_core_cancel');
+        $request->setRouteParameters(['token' => $payment->getOrder()->getTokenValue()]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports($request): bool
+    {
+        return
+            $request instanceof ResolveNextRoute &&
+            $request->getFirstModel() instanceof PaymentInterface
+        ;
+    }
+}
