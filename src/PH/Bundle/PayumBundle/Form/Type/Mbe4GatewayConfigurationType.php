7d103b58959e0b44da926ba0285df23c9f449bda
Rafał Muszyński <rmuszynski1@gmail.com>
2017-09-25 16:35:58 +0200

    fixed typo in thankyou template, updated payum-mbe4

--- src/PH/Bundle/PayumBundle/Form/Type/Mbe4GatewayConfigurationType.php
+++ src/PH/Bundle/PayumBundle/Form/Type/Mbe4GatewayConfigurationType.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PH\Bundle\PayumBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -39,6 +40,13 @@ final class Mbe4GatewayConfigurationType extends AbstractType
                 ],
             ])
             ->add('serviceId', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'groups' => 'ph',
+                    ]),
+                ],
+            ])
+            ->add('contentclass', IntegerType::class, [
                 'constraints' => [
                     new NotBlank([
                         'groups' => 'ph',
