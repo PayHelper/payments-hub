4e3ed9f7a6801b1a7df08c6353cba7a922ee5672
Rafał Muszyński <rmuszynski1@gmail.com>
2018-07-30 09:34:42 +0200

    convert old metadata to new schema

--- app/migrations/Version20180725115415.php
+++ app/migrations/Version20180725115415.php
@@ -24,8 +24,8 @@ class Version20180725115415 extends AbstractMigration
         $this->addSql('CREATE TABLE ph_subscription_metadata (id INT NOT NULL, subscription_id INT NOT NULL, metadata_key VARCHAR(255) DEFAULT NULL, metadata_value TEXT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_D5AB38299A1887DC ON ph_subscription_metadata (subscription_id)');
         $this->addSql('ALTER TABLE ph_subscription_metadata ADD CONSTRAINT FK_D5AB38299A1887DC FOREIGN KEY (subscription_id) REFERENCES ph_subscription (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('INSERT INTO ph_subscription_metadata (id, subscription_id, metadata_key, metadata_value) SELECT nextval(\'ph_subscription_metadata_id_seq\') as id, id AS subscription_id, json_data.key AS metadata_key, json_data.value AS metadata_value FROM ph_subscription, json_each_text(ph_subscription.metadata) AS json_data WHERE ph_subscription.metadata::text <> \'[]\'::text');
         $this->addSql('ALTER TABLE ph_subscription DROP metadata');
-        $this->addSql('ALTER TABLE ph_payment_method ALTER supports_recurring SET NOT NULL');
     }
 
     /**
@@ -38,7 +38,6 @@ class Version20180725115415 extends AbstractMigration
 
         $this->addSql('DROP SEQUENCE ph_subscription_metadata_id_seq CASCADE');
         $this->addSql('DROP TABLE ph_subscription_metadata');
-        $this->addSql('ALTER TABLE ph_payment_method ALTER supports_recurring DROP NOT NULL');
         $this->addSql('ALTER TABLE ph_subscription ADD metadata JSON DEFAULT NULL');
     }
 }
